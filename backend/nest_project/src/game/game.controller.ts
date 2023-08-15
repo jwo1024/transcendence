5c04ab540b86eee075d16045685f4917da183212
ssolpang <iless3377@naver.com>
2023-08-15 21:15:20 +0900

    feat/create_game_info_interface#14

--- backend/nest_project/src/game/game.controller.ts
+++ backend/nest_project/src/game/game.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { GameService } from './game.service';
 
-@Controller('game')
+@Controller('/game')
 export class GameController {
 	constructor(private readonly gameService: GameService) {}
+
+	// 큐 대기 화면
+	// 게임 화면
 
 	@Get()
 	getWindow(): string {
--- backend/nest_project/src/game/game.gateway.ts
+++ backend/nest_project/src/game/game.gateway.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
 
+// entity or dto?
+
+// 내 프로필, 상대 프로필
+// ongoing score
+// game mode: 래더, 오리지널, 커스텀(종류, 속도)
+interface GameInfo
+{
+	gameId,
+	playerLeft,
+	playerRight,
+	gameMode,
+	gameType,
+	scoreLeft: number,
+	scoreRight: number,
+}
+
+// 퐁 캔버스, 플레이어 2명의 bar, 퐁 ball
+interface GameField
+{
+	// canvas
+	barLeftY: number,
+	barRightY: number,
+	ballX: number,
+	ballY: number,
+}
+
 @Injectable()
 export class GameGateway {}
