6d34d0f774ae1839628f8b64c99cdc242b19c484
jwo1024 <dnemtmxlr@gmail.com>
2023-08-21 14:15:00 +0900

    [feat] password 설정 체크박스 활성화(시각적으로 체크표시가 적용되지 않는 문제 존재) #16

--- frontend/front/components/chat/ChatSettingBox.tsx
+++ frontend/front/components/chat/ChatSettingBox.tsx
@@ -1,6 +1,5 @@
 import React, { ReactNode } from "react";
-
-import { Fieldset, Checkbox, Input, Button, Frame } from "@react95/core";
+import { Fieldset, Checkbox, Input, Button } from "@react95/core";
 import SelectButton from "../common/SelectButton";
 
 // TMP 이걸 어떻게 처리해야하나?
@@ -13,6 +12,12 @@ interface FrameButtonProps {
 
 // ChatSettingBox
 const ChatSettingBox = ({ children }: { children?: ReactNode }) => {
+  const [checkedPassword, setCheckedPassword] = React.useState<boolean>(false);
+
+  const handleCheckedPassword = () => {
+    setCheckedPassword(!checkedPassword);
+  };
+
   const frameButton: FrameButtonProps[] = [
     { children: "공개방", handleClickCustom: () => console.log("공개방 클릭") },
     { children: "비공개방" },
@@ -20,19 +25,33 @@ const ChatSettingBox = ({ children }: { children?: ReactNode }) => {
 
   return (
     <div className="flex flex-col w-2/5 p-3">
+      
       <Fieldset
         className="flex flex-col p-2 w-full h-min gap-2 "
         legend="Chat-Room Settings"
       >
-        {/* <div>관리자만 채팅방 설정이 가능합니다.</div> */}
-        <div>공개/비공개 방</div>
-        <SelectButton frameButton={frameButton} />
-
-        <div className="flex flex-col mt-4 space-x-4">
-          <Checkbox>비밀번호 설정</Checkbox>
-          <Input placeholder="비밀번호 입력"></Input>
+        <div>관리자만 채팅방 설정이 가능합니다.</div>
+      
+        <div className="flex flex-col mt-4">
+          <div>공개/비공개 방</div>
+          <SelectButton frameButton={frameButton}/>
         </div>
+      
+        <div className="flex flex-col mt-4">
+          <Checkbox checked={checkedPassword} onChange={handleCheckedPassword}>
+            비밀번호 설정
+          </Checkbox>
+          {/* <input type="checkbox" id="scales" name="scales" checked /> */}
+          {checkedPassword ? (
+            <Input
+              placeholder="비밀번호 입력"
+              disabled={!checkedPassword}
+            ></Input>
+          ) : null}
+        </div>
+      
         <br />
+        <Button className=" font-semibold">적용하기</Button>
       </Fieldset>
     </div>
   );
