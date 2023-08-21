046bf3eedc8d97a7b98d4230474d57e1f4135f77
jwo1024 <dnemtmxlr@gmail.com>
2023-08-21 22:05:20 +0900

    [feat] MessageBox 스크롤 자동 내림 추가 #16

--- frontend/front/components/chat/MessageBox.tsx
+++ frontend/front/components/chat/MessageBox.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useRef, useEffect } from "react";
 import styled from "styled-components";
 
 import { Frame } from "@react95/core";
@@ -6,11 +6,26 @@ import NameTag from "./NameTag";
 
 interface MessageBoxProps {
   message?: string[];
+  ref?: React.RefObject<HTMLDivElement>;
 }
 // id
 
+const WhiteInnerFrame = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Frame className=" flex-1 text-sm" h="100%" boxShadow="in" bg="white">
+      {children}
+    </Frame>
+  );
+};
+
 const ChatRoomStatus: FC = () => {
   return (
+    // <Frame className="flex flex-raw p-3 text-center" h="" w="100%" padding={3}>
+    //   <WhiteInnerFrame>방제</WhiteInnerFrame>
+    //   <WhiteInnerFrame>공개방</WhiteInnerFrame>
+    //   <WhiteInnerFrame>비밀번호 없음</WhiteInnerFrame>
+    //   <WhiteInnerFrame> 메모</WhiteInnerFrame>
+    // </Frame>
     <div className="p-3">
       <span className="mx-1 my-2 bg-stone-200 p-1 rounded">방제</span>
       <span className="mx-1 my-2 bg-stone-200 p-1 rounded">공개방</span>
@@ -19,16 +34,21 @@ const ChatRoomStatus: FC = () => {
   );
 };
 
-const MessageBox: FC<MessageBoxProps> = ({ message }) => {
+const MessageBox: FC<MessageBoxProps> = ({ message, ref }) => {
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  const scrollToBottom = useEffect(() => {
+    scrollRef.current?.scrollIntoView({ behavior: "smooth" })
+  }, [message]);
+
   return (
     <Frame
-      className="flex flex-col flex-1 overflow-y-scroll scroll-behavior-smooth p-2"
+      className="flex flex-col flex-1 overflow-y-scroll p-2"
       h=""
       boxShadow="in"
       bg="white"
     >
-      {/* <Frame className="h-full" boxShadow="in" bg="white"> */}
-      <ChatRoomStatus/>
+      <ChatRoomStatus />
       {message?.map((message, index) => {
         return (
           // message overflow 처리하기
@@ -41,7 +61,7 @@ const MessageBox: FC<MessageBoxProps> = ({ message }) => {
           </div>
         );
       })}
-      {/* </Frame> */}
+      <div ref={scrollRef}/>
     </Frame>
   );
 };
