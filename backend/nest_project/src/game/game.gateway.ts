7ec0542d26e22f013df50951eca42dbfe4712be7
ssolpang <iless3377@naver.com>
2023-08-23 17:22:22 +0900

    feat/small_retouch#17

--- backend/nest_project/src/game/game.gateway.ts
+++ backend/nest_project/src/game/game.gateway.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 import { from, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { SubscribeMessage, WebSocketGateway, WebSocketServer, WsResponse } from '@nestjs/websockets';
-import { Server } from 'ws';
+import { Socket, Server } from 'socket.io';
 
 // entity or dto?
 
@@ -31,12 +31,28 @@ interface GameField
 	ballY: number,
 }
 
+// chat 소켓과의 관계:
+// 채팅방의 소켓을 그대로 가져와서 쓰기 or 게임 소켓 새로 만들기..?
+// 그대로 가져오는 게 더 맞을 것 같긴 함
+
 @Injectable()
-@WebSocketGateway({ namespace: 'game' })
+@WebSocketGateway({ namespace: 'game' }) //웹소켓 리스너 기능 부여하는 데코레이터
 export class GameGateway {
-	@WebSocketServer()
+	
+	@WebSocketServer() // 현재 동작 중인 웹소켓 서버 객체
 	server: Server;
 
+	// front side 예측도 //
+	// ladderGameButton.addEventListener("mouseclick", HandleLadderGameButton);
+	// async funcion HandleLadderGameButton(event)
+	// {
+	//		event.preventDefault();
+	//		...
+	//		socket.emit("searchMatch", (넘겨줄 데이터));
+	// 		...
+	// }
+
+	// 'localhost:3000/game' 처리
 	@SubscribeMessage('game')
-	handleEvent(){}
+	handleEvent(client: Socket, data: any){}
 }
