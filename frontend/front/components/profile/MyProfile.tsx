a7235dea4bc5928c338a14cb940ddfe8aeb8359d
Hogwon Kim <hogkim@c6r4s1.42seoul.kr>
2023-09-19 18:21:00 +0900

    feat/ onSubmitEvents #77

--- frontend/front/components/profile/MyProfile.tsx
+++ frontend/front/components/profile/MyProfile.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
   Fieldset,
   Frame,
@@ -9,6 +9,8 @@ import {
 } from "@react95/core";
 import Window from "../common/Window";
 import { UserLocalStorage } from "@/types/SignUpType";
+import sendAvatar from "../common/sendAvatar";
+import type { SignupDto, User42Dto } from "@/types/SignUpType";
 
 interface ProfileProps {
   avatarSrc: string; // 아바타 이미지 경로를 받는 속성 추가
@@ -23,12 +25,55 @@ const MyProfile: React.FC<ProfileProps> = ({ avatarSrc }) => {
     wins: 0,
     loses: 0,
   });
+
   const myProfile: boolean = true;
+  const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+
   useEffect(() => {
     const user = localStorage.getItem("user");
     const user_obj = JSON.parse(localStorage.getItem("user") || "{}");
     setMydata(user_obj);
   }, []);
+
+  const [avatarURL, setAvatarURL] = useState<string | null>(null);
+  const [uploadAvatar, setUploadAvatar] = useState<File | null>(null);
+
+  const onSubmitAvatar = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const token = sessionStorage.getItem("accessToken");
+    if (token) {
+      fetch(`${backendUrl}/auth/defaultAvatar`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+        .then((res) => {
+          if (res.ok) {
+            res.blob().then((blob) => {
+              setUploadAvatar(blob as File); // 이게 맞나 ? typeScript as
+              const url = URL.createObjectURL(blob);
+              setAvatarURL(url);
+              sendAvatar({ setAvatarURL, uploadAvatar });
+            });
+          } else {
+            console.log("확인");
+            setAvatarURL("");
+          }
+        })
+        .catch((error) => {
+          setAvatarURL("");
+          console.error("이미지를 불러오는 동안 오류 발생: ", error);
+        });
+    }
+  };
+
+  const [newNickName, setNewNickName] = useState<string | null>(null);
+
+  const onSubmitNickname = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const newNickNameInputRef = useRef<HTMLInputElement>();
+    const handleBlurInput = () => {
+      setNewNickName(newNickNameInputRef.current?.value || "");
+    };
+  };
   return (
     <Window title="My Profile" w="320" h="350">
       <div className=" flex flex-col items-center justify-between p-4">
@@ -42,7 +87,7 @@ const MyProfile: React.FC<ProfileProps> = ({ avatarSrc }) => {
       <div className="flex flex-row space-x-1 px-4">
         <form
           className="flex flex-col space-y-1 p-0.5 items-center"
-          // onSubmit={onSubmitAvatar}
+          onSubmit={onSubmitAvatar}
         >
           <Input
             placeholder="Avatar"
@@ -57,10 +102,13 @@ const MyProfile: React.FC<ProfileProps> = ({ avatarSrc }) => {
           />
           <Button className="w-full">아바타 변경</Button>
         </form>
-        <div className="flex flex-col space-y-1 p-0.5 items-center">
+        <form
+          className="flex flex-col space-y-1 p-0.5 items-center"
+          onSubmit={onSubmitAvatar}
+        >
           <Input placeholder="Nick Name" className="flex-1 w-32" />
           <Button className="w-full">닉네임 변경</Button>
-        </div>
+        </form>
       </div>
       <div className="p-4 w-full">
         <Fieldset legend="Information">
