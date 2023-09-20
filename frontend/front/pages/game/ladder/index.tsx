d1428da5126bf2a4f096715ec20c4daaeb028f97
jkwak <jkwak@student.42seoul.kr>
2023-09-20 19:50:39 +0900

    fix/ ladder front small fix #61

--- frontend/front/pages/game/ladder/index.tsx
+++ frontend/front/pages/game/ladder/index.tsx
@@ -16,23 +16,16 @@ const socket = io('http://localhost:4000/ladder_game');
 
 export default function GamePage() {
 
-  // miniprofile unit test를 위한 temp variables
-  const [left, setLeft] = useState({ nickname: "pLeft.nickname", ladder: 23, })
-  const [right, setRight] = useState({ nickname: "pRight.nickname", ladder: 42, })
+  const [left, setLeft] = useState({ nickname: "left player", ladder: 1000, win: 0, lose: 0 })
+  const [right, setRight] = useState({ nickname: "right player", ladder: 1000, win: 0, lose: 0 })
 
-  //  todo: main 화면의 래더 버튼과 연동되어야 함  or setGame() 시작 시 setMiniProfile 이벤트 발생
-  const startGameHandler = () => {
-    socket.emit('startGame');
-    socket.on('setMiniProfile', (pLeft: any, pRight: any, callback: any) => {
-      setLeft({nickname: pLeft.nickname, ladder:pLeft.level})
-      setRight({nickname: pRight.nickname, ladder:pRight.level})
-      callback();
-    });
-  };
+  socket.on('setMiniProfile', (profile1: any, profile2: any) => {
+    setLeft({nickname: profile1.nickname, ladder:profile1.level, win: profile1.wins, lose: profile1.loses})
+    setRight({nickname: profile2.nickname, ladder:profile2.level, win: profile2.wins, lose: profile2.loses})
+  });
 
   return (
     <div className="flex items-center justify-center h-screen">
-      <button onClick={startGameHandler}>START GAME</button>
       <Window title="pong game" w="900" h="850">
         <div className="h-screen flex flex-col justify-center items-center">
           <PongGame socket={socket}/>
@@ -40,16 +33,16 @@ export default function GamePage() {
             <MiniProfile
               nickname={left.nickname}
               ladder={left.ladder}
-              win={23}
-              lose={17}
+              win={left.win}
+              lose={left.lose}
               avatarSrc="https://github.com/React95.png"
             />
             <img className="h-40 mx-10" src="versus.png" />
             <MiniProfile
               nickname={right.nickname}
               ladder={right.ladder}
-              win={23}
-              lose={17}
+              win={right.win}
+              lose={right.lose}
               avatarSrc="https://github.com/React95.png"
             />
           </div>
